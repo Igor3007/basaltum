d0b37e6b35a9279996f7ead46d735849840556d8
Igor <50610898+Igor3007@users.noreply.github.com>
2023-10-24 17:22:46 +0300

    save

--- src/blocks/modules/minicard/minicard.js
+++ src/blocks/modules/minicard/minicard.js
@@ -1,4 +1,4 @@
-modules.define('main-block', ['i-bem-dom'], function(provide, bemDom) {
+modules.define('minicard', ['i-bem-dom'], function(provide, bemDom) {
 
 provide(bemDom.declBlock(this.name, {
     onSetMod: {
